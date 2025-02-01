b27059ef485be2d4dd75ffe1b7bbc8a961746467
brostyslav <bykhalrostyslav@gmail.com>
2025-02-01 13:11:52 +0200

    router links

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Home from "@/views/Home.vue";
+import Jobs from "@/views/Jobs.vue";
 
 export default createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,5 +10,10 @@ export default createRouter({
             name: 'home',
             component: Home
         },
+        {
+            path: '/jobs',
+            name: 'jobs',
+            component: Jobs
+        }
     ]
 });
