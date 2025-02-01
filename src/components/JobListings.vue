35704d42c200f21b0bc1a4d55f535f3e871e2763
brostyslav <bykhalrostyslav@gmail.com>
2025-02-01 12:43:23 +0200

    add files

---
+++ src/components/JobListings.vue
@@ -0,0 +1,36 @@
+<script setup>
+import jobData from '@/jobs.json'
+import {ref} from "vue";
+import Job from "@/components/Job.vue";
+
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const jobs = ref(jobData)
+</script>
+
+<template>
+  <section class="bg-green-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <Job v-for="job in jobs.jobs.slice(0, limit || jobs.jobs.length)" :key="job.id" :job="job"/>
+      </div>
+    </div>
+  </section>
+
+  <section class="m-auto max-w-lg my-10 px-6" v-if="showButton">
+    <a
+        href="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+    >View All Jobs</a
+    >
+  </section>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import JobListings from "@/components/JobListings.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import Hero from "@/components/Hero.vue";
+</script>
+
+<template>
+  <Hero title="Test"/>
+  <HomeCards/>
+  <JobListings :limit="3" :show-button="true"/>
+</template>
